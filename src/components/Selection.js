0ed1ed9508a4c32b57035e990c9cb9c9eb105683
Suvro96 <uniquesuvro@gmail.com>
2022-05-16 18:10:56 +0530

    task completed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React from "react";
 
+const Selection = props => {
+  const [background, setBackground] = useState({ background: "" });
+  const handleClick = () => {
+    setBackground(props.applyColor);
+  };
+  return (
+    <>
+      <div className="fix-box" onClick={handleClick}>
+        <h2 className="subheading">Selection</h2>
+      </div>
+    </>
+  );
+};
+
+export default Selection;
